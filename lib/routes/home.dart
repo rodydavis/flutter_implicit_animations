468930e8edb7ce1484b10c5b46ce41c439d914a7
Rody Davis <rody.davis.jr@gmail.com>
2022-07-15 10:34:24 -0700

    updating timing

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -13,77 +13,115 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends AnimationWidget<HomeScreen> {
+  final List<Cube> cubes = <Cube>[];
+
   double x = 0.0, y = 0.0, z = 0.0;
   Color color = Colors.red;
   Offset offset = Offset.zero;
 
+  void addCube(Size size) {
+    final randomOffset = Offset(
+      Random().nextDouble() * size.width,
+      Random().nextDouble() * size.height,
+    );
+    final randomDelta = Random().nextDouble() * 20;
+    final randomDirection = Offset(
+      Random().nextDouble() * 2 - 1,
+      Random().nextDouble() * 2 - 1,
+    );
+    final cube = Cube()
+      ..offset = randomOffset
+      ..delta = randomDelta
+      ..direction = randomDirection;
+    cubes.add(cube);
+    debugPrint('Cube added: $randomOffset');
+  }
+
+  @override
+  void start(Duration time) {
+    final size = MediaQuery.of(context).size;
+    addCube(size);
+    super.start(time);
+  }
+
   @override
   void update(Duration time) {
-    x += 0.01;
-    y += 0.01;
-    final t = time.inMilliseconds / 1000;
-    color = Color.fromARGB(
-      255,
-      (sin(t) * 127 + 128).floor(),
-      (cos(t) * 127 + 128).floor(),
-      (t * 127 + 128).floor(),
-    );
-    offset = Offset(
-      sin(t) * 100,
-      cos(t) * 100,
-    );
+    for (final cube in cubes) {
+      cube.update(time, constraints.biggest);
+    }
   }
 
   @override
   Widget paint(BuildContext context, BoxConstraints constraints) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animation Example'),
+      ),
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Positioned(
-            left: constraints.maxWidth * 0.5,
-            top: constraints.maxHeight * 0.5,
-            width: 100,
-            height: 100,
-            child: Container(
-              transform: Matrix4.identity()
-                ..rotateZ(z)
-                ..rotateY(y)
-                ..rotateX(x),
-              color: Colors.black,
-              child: const Center(
-                child: Text(
-                  'Hello World',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+          for (final cube in cubes)
+            Positioned.fromRect(
+              rect: cube.rect,
+              child: CustomPaint(
+                painter: InlinePainter(
+                  draw: (canvas, size) {
+                    final paint = Paint()
+                      ..color = cube.color
+                      ..style = PaintingStyle.fill
+                      ..strokeWidth = 2;
+                    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+                    canvas.drawRect(rect, paint);
+                  },
                 ),
               ),
             ),
-          ),
-          Positioned.fromRect(
-            rect: offset.translate(
-                  constraints.maxWidth * 0.2,
-                  constraints.maxHeight * 0.2,
-                ) &
-                const Size(100, 100),
-            child: CustomPaint(
-              painter: InlinePainter(
-                draw: (canvas, size) {
-                  final paint = Paint()
-                    ..color = color
-                    ..style = PaintingStyle.fill
-                    ..strokeWidth = 2;
-                  final rect = Rect.fromLTWH(0, 0, size.width, size.height);
-                  canvas.drawRect(rect, paint);
-                },
-              ),
-            ),
-          ),
         ],
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => addCube(constraints.biggest),
+        icon: const Icon(Icons.add),
+        label: Text('Cubes: ${cubes.length}'),
+      ),
+    );
+  }
+}
+
+class Cube {
+  Color color = Colors.black;
+  Offset offset = Offset.zero;
+  Size size = const Size(100, 100);
+  double delta = 10;
+  Offset direction = const Offset(0.1, 0.4);
+  Rect get rect => offset & size;
+
+  void update(Duration time, Size size) {
+    // Move cube and change direction if out of bounds
+    offset = offset + direction * delta;
+
+    // Top
+    if (offset.dy < 0) {
+      direction = Offset(direction.dx, 1);
+    }
+    // Bottom
+    if (offset.dy > size.height) {
+      direction = Offset(direction.dx, -1);
+    }
+    // Left
+    if (offset.dx < 0) {
+      direction = Offset(1, direction.dy);
+    }
+    // Right
+    if (offset.dx > size.width) {
+      direction = Offset(-1, direction.dy);
+    }
+
+    // Change color
+    color = Color.fromARGB(
+      255,
+      (offset.dx * 255 / size.width).round(),
+      (offset.dy * 255 / size.height).round(),
+      0,
     );
   }
 }
--- lib/widgets/animation.dart
+++ lib/widgets/animation.dart
@@ -4,17 +4,21 @@ import 'package:flutter/scheduler.dart';
 abstract class AnimationWidget<T extends StatefulWidget> extends State<T>
     with SingleTickerProviderStateMixin {
   Duration elapsed = Duration.zero;
+  Duration delta = Duration.zero;
   late final Ticker ticker;
+  BoxConstraints constraints = const BoxConstraints.tightFor();
 
   @override
   void initState() {
     super.initState();
     ticker = createTicker((elapsed) {
+      delta = elapsed - this.elapsed;
       this.elapsed = elapsed;
       update(elapsed);
       if (mounted) setState(() {});
     });
     ticker.start();
+    WidgetsBinding.instance.addPostFrameCallback(start);
   }
 
   @override
@@ -24,7 +28,14 @@ abstract class AnimationWidget<T extends StatefulWidget> extends State<T>
   }
 
   @override
-  Widget build(BuildContext context) => LayoutBuilder(builder: paint);
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, dimens) {
+      constraints = dimens;
+      return paint(context, dimens);
+    });
+  }
+
+  void start(Duration time) {}
 
   void update(Duration time);
 
